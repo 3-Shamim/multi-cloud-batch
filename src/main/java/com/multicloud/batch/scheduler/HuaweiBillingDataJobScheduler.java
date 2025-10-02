2478845f6374c83df6e088ec607fd92216c663ca
Shamim <shamim.molla@vivasoftltd.com>
2025-10-02 16:01:18 +0600

    scheduler added for external huawei data job

--- src/main/java/com/multicloud/batch/scheduler/HuaweiBillingDataJobScheduler.java
+++ src/main/java/com/multicloud/batch/scheduler/HuaweiBillingDataJobScheduler.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,26 +21,40 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@ConditionalOnExpression("${batch_job.huawei_billing_data.enabled}")
+@ConditionalOnExpression("${batch_job.huawei_billing_data.enabled} OR ${batch_job.external_huawei_billing_data.enabled}")
 public class HuaweiBillingDataJobScheduler {
+
+    @Value("${batch_job.huawei_billing_data.enabled}")
+    private boolean huaweiBillingDataJobEnabled;
+    @Value("${batch_job.external_huawei_billing_data.enabled}")
+    private boolean externalHuaweiBillingDataJobEnabled;
 
     private final JobLauncher jobLauncher;
     private final JobService jobService;
     private final Job huaweiBillingDataJob;
+    private final Job externalHuaweiBillingDataJob;
 
     public HuaweiBillingDataJobScheduler(JobLauncher jobLauncher,
                                          JobService jobService,
                                          @Qualifier("huaweiBillingDataJob")
-                                         Job huaweiBillingDataJob) {
+                                         Job huaweiBillingDataJob,
+                                         @Qualifier("externalHuaweiBillingDataJob")
+                                         Job externalHuaweiBillingDataJob) {
 
         this.jobLauncher = jobLauncher;
         this.jobService = jobService;
         this.huaweiBillingDataJob = huaweiBillingDataJob;
+        this.externalHuaweiBillingDataJob = externalHuaweiBillingDataJob;
     }
 
     @Async
     @Scheduled(cron = "${batch_job.huawei_billing_data.corn}")
     public void runHuaweiBillingDataJob() throws Exception {
+
+        if (!huaweiBillingDataJobEnabled) {
+            log.info("Skipping because the job huaweiBillingDataJob is disabled");
+            return;
+        }
 
         if (jobService.isJobTrulyRunning(huaweiBillingDataJob.getName())) {
             log.info(
@@ -54,6 +69,32 @@ public class HuaweiBillingDataJobScheduler {
                 .toJobParameters();
 
         jobLauncher.run(huaweiBillingDataJob, jobParameters);
+
+    }
+
+
+    @Async
+    @Scheduled(cron = "${batch_job.external_huawei_billing_data.corn}")
+    public void runExternalHuaweiBillingDataJob() throws Exception {
+
+        if (!externalHuaweiBillingDataJobEnabled) {
+            log.info("Skipping because the job externalHuaweiBillingDataJob is disabled");
+            return;
+        }
+
+        if (jobService.isJobTrulyRunning(externalHuaweiBillingDataJob.getName())) {
+            log.info(
+                    "Skipping externalHuaweiBillingDataJob because the job is already running: {}",
+                    externalHuaweiBillingDataJob.getName()
+            );
+            return;
+        }
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+
+        jobLauncher.run(externalHuaweiBillingDataJob, jobParameters);
 
     }
 
