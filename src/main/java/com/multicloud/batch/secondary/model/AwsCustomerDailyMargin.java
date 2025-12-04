8218437ca61d33ca32c67002941f7460c18e32a4
Shamim <shamim.molla@vivasoftltd.com>
2025-12-04 17:57:24 +0600

    need to be completed

---
+++ src/main/java/com/multicloud/batch/secondary/model/AwsCustomerDailyMargin.java
@@ -0,0 +1,49 @@
+package com.multicloud.batch.secondary.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(
+        name = "aws_customer_daily_margin",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "idx_uq_const", columnNames = {"day", "customer_name"})
+        }
+)
+public class AwsCustomerDailyMargin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private LocalDate day;
+
+    @Column(name = "customer_name", nullable = false, length = 100)
+    private String customerName;
+
+    @Column(name = "azerion_cost", precision = 20, scale = 8)
+    private BigDecimal azerionCost;
+
+    @Column(name = "customer_cost", precision = 20, scale = 8)
+    private BigDecimal customerCost;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean external;
+
+}
---
+++ src/main/java/com/multicloud/batch/secondary/repository/AwsCustomerDailyMarginRepository.java
@@ -0,0 +1,17 @@
+package com.multicloud.batch.secondary.repository;
+
+import com.multicloud.batch.secondary.model.AwsCustomerDailyMargin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Md. Shamim Molla
+ * Email: shamim.molla@vivasoftltd.com
+ */
+
+@Repository
+public interface AwsCustomerDailyMarginRepository extends JpaRepository<AwsCustomerDailyMargin, Long> {
+
+
+}
